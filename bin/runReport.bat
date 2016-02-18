7e039e805f498f9374193ba0287b2975e91e195c
ps073006 <engr.ps07@gmail.com>
2016-02-18 15:33:19 -0600

    repGen
    repGen

---
+++ bin/runReport.bat
@@ -0,0 +1,7 @@
+javac -cp .asm-all-5.0.4.jar;org.jacoco.report-0.7.6.201601131008.jar;org.jacoco.core-0.7.6.201601131008.jar;org.jacoco.ant-0.7.6.201601131008.jar;org.jacoco.agent-0.7.6.201601131008.jar ReportGenerator/ReportGenerator.java
+
+pause;
+
+java  -cp .;asm-all-5.0.4.jar;org.jacoco.report-0.7.6.201601131008.jar;org.jacoco.core-0.7.6.201601131008.jar;org.jacoco.ant-0.7.6.201601131008.jar;org.jacoco.agent-0.7.6.201601131008.jar ReportGenerator.ReportGenerator C:\Users\ps\workspace\testJava
+
+pause;
